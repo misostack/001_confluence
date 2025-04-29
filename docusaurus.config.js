b212429e67183f0029a231e70a341166e0f05921
misostack <misostack.com@gmail.com>
2025-04-29 08:53:19 +0700

    Change tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Refactor Everything",
-  tagline: "Nothing is difficult, only fear is not persistent",
+  tagline: "Refactor today, lead tomorrow.",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
